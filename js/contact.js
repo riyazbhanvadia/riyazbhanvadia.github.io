1fa863105289781079d2f725bc7ba191753207ee
riyazbhanvadia-innoplexus <riyaz.bhanvadia@innoplexus.com>
2017-05-02 10:18:41 +0530

    Form submitted done

--- js/contact.js
+++ js/contact.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             action += (action.indexOf('?') !== -1) ? "&" : "?";
             $.getJSON(action + $form.serialize() + "&callback=?").then(function(res) {
                 if (res === "OK") {
-                    $form.html('<i class="fa fa-check-circle" aria-hidden="true"></i><h3 class="mz-sucess"><i class="fa fa-smile-o" aria-hidden="true"></i>&nbsp;Thank you! You should receive a confirmation shortly.</h3');
+                    $form.html('<i class="fa fa-check-circle" aria-hidden="true"></i><h3 class="mz-sucess"><i class="fa fa-smile-o" aria-hidden="true"></i>&nbsp;Thank you! I will respond to your mail as soon as possible.</h3');
                 } else {
                     $form.append('<span class="mz-validationmessage">Sorry, an error occurred. Please make sure all fields are filled out!</span>');
                 }
